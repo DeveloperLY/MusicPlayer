4a630b722399971e488b7aa0ff652366b44cacee
LY <coderyliu@gmail.com>
2017-03-27 20:22:12 +0800

    修改列表动画效果

--- MusicPlayer/MusicPlayer/Classes/MusicList/Controller/LYListTableViewController.m
+++ MusicPlayer/MusicPlayer/Classes/MusicList/Controller/LYListTableViewController.m
@@ -106,14 +106,6 @@
     // 取出数据模型赋值
     LYMusicModel *musicModel = self.musicModels[indexPath.row];
     
-    // 如果想要做一些比较炫的动画效果, 可以在这
-    [cell.layer removeAnimationForKey:@"cell"];
-    CAKeyframeAnimation *animation = [[CAKeyframeAnimation alloc] init];
-    animation.keyPath = @"transform.rotation.z";
-    animation.values = @[@(1), @(0), @(-1), @(0)];
-    animation.duration = 0.5;
-    [cell.layer addAnimation:animation forKey:@"cell"];
-    
     cell.musicModel = musicModel;
     
     return cell;
@@ -133,6 +125,17 @@
     // 跳转到详情界面
     [self performSegueWithIdentifier:@"list2Detail" sender:nil];
     
+}
+
+// cell动画
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    // 设置Cell的动画效果为3D效果
+    // 设置x和y的初始值为0.1；
+    cell.layer.transform = CATransform3DMakeScale(0.1, 0.1, 1);
+    // x和y的最终值为1
+    [UIView animateWithDuration:1 animations:^{
+        cell.layer.transform = CATransform3DMakeScale(1, 1, 1);
+    }];
 }
 
 @end
